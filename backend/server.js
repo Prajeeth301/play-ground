7dd23cd2c7094f24edf282da446e7d915524d8c9
Prajeeth301 <prajeeth.a@nuncsystems.com>
2025-05-08 23:27:00 +0530

    fix: backend ci cd trail4

--- backend/server.js
+++ backend/server.js
@@ -49,6 +49,6 @@ app.all('/', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log("backend: CI/CD Trail : 3");
+    console.log("backend: CI/CD Trail : 4");
     console.log(`Server running on port ${PORT}`);
 });
